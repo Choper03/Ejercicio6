ceaf896b909f30cd55aa3bb9c3bc23e86b2be857
Choper03 <csutucdeleon@gmail.com>
2023-07-28 20:29:40 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var total1 = 0;
+
+function sumar(){
+    total1 +=1;
+    document.getElementById("dato1").textContent = total1;
+}
+
+function restar(){
+    total1 -=1;
+    document.getElementById("dato1").textContent = total1;
+}
